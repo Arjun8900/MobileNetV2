f446a747593c9370c45d4e2351f02fad4f0acb70
Arjun8900 <kanwal.arjun8900@gmail.com>
2019-01-31 11:05:29 +0530

    Update mv2_prediction.py

--- mv2_prediction.py
+++ mv2_prediction.py
@@ -16,7 +16,7 @@ from keras.applications import imagenet_utils
 model = MobileNetV2(weights='imagenet', include_top=True)
 
 
-path = 'cat.4059.jpg'
+path = 'cat.4059.jpg'   # Replace with custom image path
 
 img = cv2.resize(cv2.imread(path,1), (224,224))
 img = np.expand_dims(img, axis=0)
